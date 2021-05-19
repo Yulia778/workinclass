7ea42e13fbf8acf4ce2b80e6195f226b2f910d11
antonyourkevich09@tut.by <antonyourkevich09@tut.by>
2021-05-19 20:19:13 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+
+        int a = 2357;
+        int b = 10;
+        int c = a % b;
+        if (c == 7) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+
+
+        // найти факториал 10
+        int i =1;
+        int sum =1;
+        for (;i<10; i++){
+            sum = sum *i;
+        }
+        System.out.println(sum);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+//определить остаток от деления 7; факториал 3! = 1*2*3...
